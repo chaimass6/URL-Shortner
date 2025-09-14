c2866b0feaf3117a8c9ffa420f588c90ce634e8b
chaimass6 <chaitu.6914@gmail.com>
2025-09-15 00:01:25 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const mongoose = require ('mongoose');
-const cors = require ('cors');
-const { nanoid } = require ('nanoid');
-require ('dotenv'). config();
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { nanoid } from 'nanoid';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
 
 const app = express();
 app.use (cors());
 app.use (express.json());
 
+
 mongoose.connect(process.env.DATABASE_URL)
-.then(() => console.log('DB Connected'))
-.catch((err) => console.log('DB Connection Error: ', err));
+    
+    .then(() => console.log('DB Connected'))
+    .catch((err) => console.log('DB Connection Error: ', err));
 
 app.listen(5000, () => console.log('Server running on port 5000'));
-
 
