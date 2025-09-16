19f407d17e509294ceae6aa18d0b58b0a43dd1ec
chaimass6 <chaitu.6914@gmail.com>
2025-09-16 22:10:04 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,8 @@ app.use (express.json());
 
 
 
+
+
 mongoose.connect(process.env.DATABASE_URL)
     .then(() => console.log('DB Connected'))
     .catch((err) => console.log('DB Connection Error: ', err));
